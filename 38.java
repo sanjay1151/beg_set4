95cd1d06d5277bb5988dc254c4e22ef57cfb6d81
sanjay1151 <35390831+sanjay1151@users.noreply.github.com>
2018-03-14 20:57:41 +0530

    Create 38.java

---
+++ 38.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class MyClass 
+{
+    public static void main(String args[]) 
+    {
+        Scanner sc = new Scanner (System.in);
+        int n1 = sc.nextInt();
+        int n2 = sc.nextInt();
+        n1 = n1 + n2;
+        n2 = n1 - n2;
+        n1 = n1 - n2;
+        System.out.println(n1 + " " + n2);
+        
+    }
+}
